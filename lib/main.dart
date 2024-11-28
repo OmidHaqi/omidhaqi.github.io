b50f8ffdd89c31ea7807940616d840830c87ed2d
Umut <omid.haqi@outlook.com>
2024-11-28 13:45:41 +0330

    Refactor main.dart to update app title and enhance UI with a new background and styled text

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Omid Haqi',
+    return MaterialApp(
+      title: 'ox0',
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
-          child: Text("Hello World!"),
+          child: Opacity(
+            opacity: 0.25,
+            child: Container(
+              width: 559,
+              height: 324,
+              decoration: ShapeDecoration(
+                
+                gradient: LinearGradient(
+                  begin: const Alignment(0.87, -0.50),
+                  end: const Alignment(-0.87, 0.5),
+                  colors: [
+                    Colors.white.withOpacity(0.30000001192092896),
+                    Colors.white.withOpacity(0.05000000074505806)
+                  ],
+                ),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.white.withOpacity(0.50),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                  
+                ),
+              ),
+              child: const Center(
+                child: Text(
+                  'HelloWorld',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
