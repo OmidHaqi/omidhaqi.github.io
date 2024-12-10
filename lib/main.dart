7499fc07eab9e7a6ed6ea87f2dc47ca38aa26d82
Umut <omid.haqi@outlook.com>
2024-12-10 17:50:12 +0330

    Enable path URL strategy for improved routing in the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'package:ox0/core/common/blocs/desktop_nav_bar_route_cubit/desktop_nav_bar_route_cubit.dart';
 import 'package:ox0/locator.dart';
@@ -9,6 +10,8 @@ main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initLocator();
+
+  usePathUrlStrategy();
 
   runApp(
     MultiBlocProvider(
