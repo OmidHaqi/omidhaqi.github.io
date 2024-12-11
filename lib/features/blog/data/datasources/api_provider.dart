7c2b18e413f72af738d6591f7c2ca6477fff85be
Umut <omid.haqi@outlook.com>
2024-12-11 15:34:49 +0330

    Add method to retrieve a blog post by slug and streamline post list fetching

--- lib/features/blog/data/datasources/api_provider.dart
+++ lib/features/blog/data/datasources/api_provider.dart
@@ -7,26 +7,25 @@ import 'package:pocketbase/pocketbase.dart';
 class ApiProvider {
   final pb = PocketBase(AppConstants.baseUrl);
 
-
   List<BlogModel> postList = [];
 
   getPosts() async {
     try {
-
-      final records =
-          await pb.collection('blogs').getFullList(sort: '-created');
-
-      List<BlogModel> postsList = records
-          .map(
-            (post) => BlogModel.fromJson(
-              post.toJson(),
-            ),
-          )
-          .toList();
-
+      final records = await pb.collection('blogs').getFullList(sort: '-created');
+      List<BlogModel> postsList = records.map((post) => BlogModel.fromJson(post.toJson())).toList();
       postList = postsList;
     } catch (e) {
       log(e.toString());
+    }
+  }
+
+  Future<BlogModel?> getPostBySlug(String slug) async {
+    try {
+      final record = await pb.collection('blogs').getFirstListItem('slug="$slug"');
+      return BlogModel.fromJson(record.toJson());
+    } catch (e) {
+      log(e.toString());
+      return null;
     }
   }
 }
