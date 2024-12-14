5c74a4e41846d5aa76e3aa5bec7b0c6caaf72337
Umut <omid.haqi@outlook.com>
2024-12-14 16:17:14 +0330

    Add padding parameter to AppContainer widget and update color opacity method

--- lib/core/common/widgets/app_continer.dart
+++ lib/core/common/widgets/app_continer.dart
@@ -4,6 +4,7 @@ class AppContiner extends StatelessWidget {
   final double? width;
   final double? height;
   final EdgeInsetsGeometry? margin;
+  final EdgeInsetsGeometry? padding;
   final Widget child;
   final double borderRadius = 30;
   final double borderWidth = 1;
@@ -14,7 +15,7 @@ class AppContiner extends StatelessWidget {
     this.height,
     this.onTap,
     required this.child,
-    this.margin,
+    this.margin, this.padding,
   });
 
   @override
@@ -25,19 +26,20 @@ class AppContiner extends StatelessWidget {
         width: width,
         height: height,
         margin: margin,
+        padding: padding,
         foregroundDecoration: ShapeDecoration(
           gradient: LinearGradient(
             begin: const Alignment(0.87, -0.50),
             end: const Alignment(-0.87, 0.5),
             colors: [
-              Colors.white.withOpacity(0.07),
-              Colors.white.withOpacity(0.02)
+              Colors.white.withValues(alpha: 0.07),
+              Colors.white.withValues(alpha: 0.02)
             ],
           ),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(borderRadius),
             side: BorderSide(
-              color: Colors.white.withOpacity(0.10),
+              color: Colors.white.withValues(alpha: 0.10),
               width: borderWidth,
               style: BorderStyle.solid,
             ),
