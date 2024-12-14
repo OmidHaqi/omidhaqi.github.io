1700aa283af8a187228a72bf8234f3af3a41dbdd
Umut <omid.haqi@outlook.com>
2024-12-14 16:17:40 +0330

    Refactor LoadingWidget to remove unnecessary Scaffold wrapper

--- lib/core/common/widgets/loading_widget.dart
+++ lib/core/common/widgets/loading_widget.dart
@@ -8,32 +8,30 @@ class LoadingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Shimmer.fromColors(
-          baseColor: Colors.white,
-          highlightColor: Theme.of(context).colorScheme.primary,
-          child: Column(
-            spacing: 10,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 300,
-                height: 20,
-                color: Colors.white,
-              ),
-              Container(
-                width: 250,
-                height: 20,
-                color: Colors.white,
-              ),
-              Container(
-                width: 200,
-                height: 20,
-                color: Colors.white,
-              ),
-            ],
-          ),
+    return Center(
+      child: Shimmer.fromColors(
+        baseColor: Colors.white,
+        highlightColor: Theme.of(context).colorScheme.primary,
+        child: Column(
+          spacing: 10,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 20,
+              color: Colors.white,
+            ),
+            Container(
+              width: 250,
+              height: 20,
+              color: Colors.white,
+            ),
+            Container(
+              width: 200,
+              height: 20,
+              color: Colors.white,
+            ),
+          ],
         ),
       ),
     );
