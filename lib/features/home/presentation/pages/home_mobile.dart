99ecf7687d1b632c51c81ae0ddb5b2c9bc78a670
Umut <omid.haqi@outlook.com>
2024-12-16 17:29:51 +0330

    Add MobileAppBar and MyDrawer to HomeMobile; implement drawer items for improved navigation

--- lib/features/home/presentation/pages/home_mobile.dart
+++ lib/features/home/presentation/pages/home_mobile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ox0/core/common/utils/screen_utils.dart';
 import 'package:ox0/core/common/widgets/app_continer.dart';
+import 'package:ox0/core/common/widgets/mobile_app_bar.dart';
+import 'package:ox0/core/common/widgets/my_drawer.dart';
+import 'package:ox0/core/common/widgets/my_drawer_item.dart';
 
 class HomeMobile extends StatelessWidget {
   const HomeMobile({
@@ -9,7 +12,35 @@ class HomeMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
+      appBar: MobileAppBar(size: size),
+      drawer: MyDrawer(
+        ctaText: 'Let\'s Talk',
+        onPressedCTA: () {},
+        children: [
+          MyDrawerItem(
+            title: 'Home',
+            onTap: () {},
+          ),
+          MyDrawerItem(
+            title: 'About',
+            onTap: () {},
+          ),
+          MyDrawerItem(
+            title: 'Works',
+            onTap: () {},
+          ),
+          MyDrawerItem(
+            title: 'Blogs',
+            onTap: () {},
+          ),
+          MyDrawerItem(
+            title: 'Contact',
+            onTap: () {},
+          ),
+        ],
+      ),
       body: Center(
         child: AppContainer(
           width: 350.dp(context),
