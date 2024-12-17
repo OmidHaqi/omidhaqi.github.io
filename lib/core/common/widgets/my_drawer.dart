fc59fcaf9ccf4e6c33d84d6925077bbbeb1a773b
Umut <omid.haqi@outlook.com>
2024-12-17 17:54:09 +0330

    Add navigation items to MyDrawer for improved accessibility and user experience

--- lib/core/common/widgets/my_drawer.dart
+++ lib/core/common/widgets/my_drawer.dart
@@ -1,15 +1,19 @@
 import 'package:blurbox/blurbox.dart';
 import 'package:flutter/material.dart';
 import 'package:ox0/core/common/widgets/logo.dart';
+import 'package:ox0/core/common/widgets/my_drawer_item.dart';
+import 'package:ox0/features/about/presentation/pages/about_screen.dart';
+import 'package:ox0/features/blog/presentation/pages/blog/blog_screen.dart';
+import 'package:ox0/features/contact/presentation/pages/contact_screen.dart';
+import 'package:ox0/features/home/presentation/pages/home_screen.dart';
+import 'package:ox0/features/works/presentation/pages/works_screen.dart';
 import 'app_button.dart'; // Add this import
 
 class MyDrawer extends StatelessWidget {
-  final List<Widget> children;
   final VoidCallback? onPressedCTA;
   final String ctaText;
   const MyDrawer({
     super.key,
-    required this.children,
     this.onPressedCTA,
     required this.ctaText,
   });
@@ -33,7 +37,36 @@ class MyDrawer extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              ...children,
+              MyDrawerItem(
+                title: 'Home',
+                onTap: () {
+                  Navigator.pushNamed(context, HomeScreen.routeName);
+                },
+              ),
+              MyDrawerItem(
+                title: 'About',
+                onTap: () {
+                  Navigator.pushNamed(context, AboutScreen.routeName);
+                },
+              ),
+              MyDrawerItem(
+                title: 'Works',
+                onTap: () {
+                  Navigator.pushNamed(context, WorksScreen.routeName);
+                },
+              ),
+              MyDrawerItem(
+                title: 'Blogs',
+                onTap: () {
+                  Navigator.pushNamed(context, BlogScreen.routeName);
+                },
+              ),
+              MyDrawerItem(
+                title: 'Contact',
+                onTap: () {
+                  Navigator.pushNamed(context, ContactScreen.routeName);
+                },
+              ),
               const SizedBox(
                 height: 20,
               ),
@@ -41,7 +74,7 @@ class MyDrawer extends StatelessWidget {
                 onPressed: onPressedCTA,
                 text: ctaText,
               ),
-             const Text('Made with ☕ by Umut'),
+              const Text('Made with ☕ by Umut'),
             ],
           ),
         ),
