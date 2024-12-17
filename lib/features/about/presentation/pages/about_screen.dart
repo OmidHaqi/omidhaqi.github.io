738984a84e8212bf4d6db83dba423aa80d248883
Umut <omid.haqi@outlook.com>
2024-12-18 00:02:40 +0330

    Wrap AboutScreen in BlocProvider to manage state with AboutBloc and fetch data on initialization

--- lib/features/about/presentation/pages/about_screen.dart
+++ lib/features/about/presentation/pages/about_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ox0/core/common/utils/responsive.dart';
+import 'package:ox0/features/about/data/datasources/about_api_provider.dart';
+import 'package:ox0/features/about/presentation/bloc/about_bloc.dart';
 import 'package:ox0/features/about/presentation/pages/about_desktop.dart';
 import 'package:ox0/features/about/presentation/pages/about_mobile.dart';
 import 'package:ox0/features/about/presentation/pages/about_tablet.dart';
@@ -10,12 +13,22 @@ class AboutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (Responsive.isDesktop(context)) {
-      return const AboutDesktop();
-    } else if (Responsive.isTablet(context)) {
-      return const AboutTablet();
-    } else {
-      return const AboutMobile();
-    }
+    return BlocProvider(
+      create: (context) => AboutBloc(AboutApiProvider())
+        ..add(
+          FetchAboutItem(),
+        ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (Responsive.isDesktop(context)) {
+            return const AboutDesktop();
+          } else if (Responsive.isTablet(context)) {
+            return const AboutTablet();
+          } else {
+            return const AboutMobile();
+          }
+        },
+      ),
+    );
   }
 }
