485411208fb56373e7bf7ea8ca8f3a27460bc025
Umut <omid.haqi@outlook.com>
2025-01-03 13:56:17 +0330

    Add localization support with flutter_localizations package

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:ox0/core/config/app_constants.dart';
 import 'package:ox0/core/config/app_theme.dart';
 import 'package:ox0/features/home/presentation/pages/home_screen.dart';
@@ -11,6 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('fa'), // Pesrian
+      ],
+      locale: const Locale('en'),
       title: AppConstants.appName,
       theme: AppTheme.darkTheme,
       initialRoute: HomeScreen.routeName,
