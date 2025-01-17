0d0152392d3375d93c6c3c2df1fcb9bd89427a89
Umut <omid.haqi@outlook.com>
2025-01-17 16:59:02 +0330

    Update service worker version, modify website description, and enhance meta tags

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2483953930"
+    serviceWorkerVersion: "3905423931"
   }
 });
